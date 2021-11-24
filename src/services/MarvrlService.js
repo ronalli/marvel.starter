504ccf94ba0016763cc331b18c0e768da6a034a6
Nikita <khakimov.nikita.vch@gmail.com>
2021-11-24 08:13:09 +0200

    transformDescription

--- src/services/MarvrlService.js
+++ src/services/MarvrlService.js
@@ -24,10 +24,19 @@ class MarvelService {
 	_transformCharacter = (char) => {
 		return {
 			name: char.name,
-			description: char.description,
+			description: this.transformDescription(char.description),
 			thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
 			homepage: char.urls[0].url,
 			wiki: char.urls[1].url
+		}
+	}
+
+	transformDescription = (str) => {
+		if (str.length === 0) {
+			return `К сожалению, описание об персонаже отсутствует. Приносим извинения, за доставленные неудобства.`
+		}
+		if (str.length > 230) {
+			return str.slice(0, 200) + '...'
 		}
 	}
 
